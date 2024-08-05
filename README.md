2932ad4ff6775b7ae148a2057ffabf949922da84
Miguel Zacca <miguel_zacca@estudante.sesisenai.org.br>
2024-08-05 09:47:06 -0300

    v1.1.0

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # Super sessionStorage
 
-Implementation of `window.sessionStorage` for server.
+Implementation of improved `window.sessionStorage` for server, `Type-Safe`, and TTL cache system.
 
-it works without the need for a `window`. Furthermore, super sessionstorage maintains the original types, avoiding the need to make conversions. (`type-safe storage`)
+it works without the need for a `window`. Furthermore, super sessionstorage maintains the original types, avoiding the need to make conversions. (type-safe storage).
 
 ## Install
 
@@ -18,33 +18,49 @@ npm install super-sessionstorage
 
 - `key`
 
+- `includes`
+
 - `clear`
 
 - `length`
 
 - `removeItem`
 
+## Options
+
+- `ttl` default is infinity (seconds)
+
+- `checkperiod` default is 60 (seconds)
+
 ## Example
+
+config.js:
+
+```js
+import { SuperSessionStorage } from 'super-sessionstorage'
+
+export const storage = new SuperSessionStorage({ ttl: 3600 })
+```
 
 setItem:
 
 ```js
-import superSessionStorage from 'super-sessionstorage'
+import { storage } from './config.js'
 
-const myObject = { 
-  test1: 123, 
-  test2: [1, 2, '123'] 
+const myObject = {
+  test1: 123,
+  test2: [1, 2, '123'],
 }
 
-superSessionStorage.setItem('example', myObject)
+storage.setItem('example', myObject)
 ```
 
 getItem:
 
 ```js
-import superSessionStorage from 'super-sessionstorage'
+import { storage } from './config.js'
 
-const example = superSessionStorage.getItem('example')
+const example = storage.getItem('example')
 
 console.log(example)
 console.log(typeof example)
--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,112 @@
 interface SuperSessionStorageProperties {
   setItem(key: string, value: any): void
   getItem(key: string): any
-  key(index: number): any
+  key(index: number): string | undefined
+  has(key: string): boolean
+  includes(value: any): boolean
   removeItem(key: string): void
   clear(): void
   get length(): number
 }
 
-class SuperSessionStorage implements SuperSessionStorageProperties {
-  private map = new Map()
+interface SuperSessionStorageConstructor {
+  ttl: number
+  checkperiod?: number
+}
+
+type MapEntry = { value: any; expiresIn: number | undefined }
+
+export class SuperSessionStorage implements SuperSessionStorageProperties {
+  private map = new Map<string, MapEntry>()
+
+  private ttl?: number
+  private cleanupInterval?: number
+
+  constructor(options?: SuperSessionStorageConstructor) {
+    if (options?.ttl) {
+      this.ttl = options.ttl
+      this.cleanupInterval = setInterval(
+        () => this.cleanupExpiredEntries(),
+        (options.checkperiod || 60) * 1000,
+      )
+    }
+  }
+
+  private get getCurrentTime() {
+    return new Date().getTime() / 1000
+  }
+
+  private getUnexpiredItem(key: string) {
+    const entry = this.map.get(key)
+
+    if (entry?.expiresIn && this.getCurrentTime > entry.expiresIn) {
+      this.map.delete(key)
+      return
+    }
+    return entry
+  }
+
+  private cleanupExpiredEntries() {
+    const currentTime = this.getCurrentTime
+
+    for (const [key, entry] of this.map) {
+      if (entry.expiresIn && currentTime > entry.expiresIn) {
+        this.map.delete(key)
+      }
+    }
+  }
+
+  private isEqual(obj1: any, obj2: any) {
+    if (typeof obj1 !== 'object') {
+      return obj1 === obj2
+    }
+
+    const keys1 = Object.keys(obj1)
+    const keys2 = Object.keys(obj2)
+
+    if (keys1.length !== keys2.length) {
+      return false
+    }
+
+    for (const key of keys1) {
+      if (!keys2.includes(key) || !this.isEqual(obj1[key], obj2[key])) {
+        return false
+      }
+    }
+
+    return true
+  }
 
   setItem(key: string, value: any): void {
-    this.map.set(key, value)
+    const expiresIn = this.ttl && this.getCurrentTime + this.ttl
+    this.map.set(key, { value, expiresIn })
   }
 
   getItem(key: string) {
-    return this.map.get(key)
+    return this.getUnexpiredItem(key)?.value
   }
 
-  key(index: number) {
-    return Array.from(this.map.entries())[index][1]
+  key(index: number): string | undefined {
+    this.cleanupExpiredEntries()
+    const entries = Array.from(this.map.entries())
+    return entries[index] ? entries[index][0] : undefined
+  }
+
+  has(key: string): boolean {
+    const entry = this.getUnexpiredItem(key)
+    return !!entry
+  }
+
+  includes(value: any): boolean {
+    this.cleanupExpiredEntries()
+    const entries = Array.from(this.map.values())
+
+    for (const entry of entries) {
+      if (this.isEqual(value, entry.value)) {
+        return true
+      }
+    }
+    return false
   }
 
   removeItem(key: string): void {
@@ -28,12 +115,11 @@ class SuperSessionStorage implements SuperSessionStorageProperties {
 
   clear(): void {
     this.map.clear()
+    clearInterval(this.cleanupInterval)
   }
 
   get length(): number {
+    this.cleanupExpiredEntries()
     return this.map.size
   }
 }
-
-const superSessionStorage = new SuperSessionStorage()
-export default superSessionStorage
